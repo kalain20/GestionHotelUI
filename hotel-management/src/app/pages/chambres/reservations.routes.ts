80099ca3bec1511adcb40595fc827a5eb2526675
Kouame Alain N'Gouandi <kalain20@yahoo.fr>
2026-04-06 03:57:24 -0400

    Suppression de ficheirs inutiles dans le compasant chambres

--- hotel-management/src/app/pages/chambres/reservations.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Route } from '@angular/router';
-import { ReservationComponent } from './reservations.component';
-
-export const RESERVATIONS: Route[]= [{path:'' , component : ReservationComponent}]
